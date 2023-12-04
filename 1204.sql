6907c42b8657f177d8217b88fb5076894538e357
blockenters <58394003+blockenters@users.noreply.github.com>
2023-12-04 17:33:07 +0900

    Add files via upload

--- 1204.sql
+++ 1204.sql
@@ -74,7 +74,60 @@ where userId = 1;
 -- 위의 둘을, 한번에 처리하는 SQL 
 update schedule 
 set isCompleted =  if(isCompleted = 0, 1, 0)
-where id = 41 and userId = 1
+where id = 41 and userId = 1;
+
+
+-- 영화 리뷰 서비스 
+use movie_db;
+
+select * from movie;
+
+-- 회원가입 SQL 
+insert into user
+(email, password, nickname, gender)
+values
+('aaa@naver.com', '1234', '홍길동' , 1);
+
+select * from user;
+
+-- 리뷰 작성 SQL  
+select * from review;
+
+insert into review
+(movieId, userId, rating, content)
+values
+(3, 1, 4, '재밌어요');
+
+-- 영화 상세정보 화면에 필요한 SQL  
+-- 가정 : 영화 아이디가 2인 영화의 상세 정보. 
+select m.* , avg(r.rating) as rating_avg , count(r.rating) review_count
+from movie m
+left join review r
+on m.id = r.movieId
+where m.id = 2;
+
+select * from user;
+
+-- 특정 영화에 대한 리뷰 가져오는 SQL
+
+
+-- 내정보 화면에서,  내가 작성한 리뷰 리스트 가져오는 SQL 
+
+
+-- 특정 영화를 선택하면, 즐겨찾기에 저장하는 SQL 
+-- 가정 : 영화아이디 1번을 즐겨찾기 하거나 해제하는 것 
+
+
+-- 위에서 즐겨찾기 한 영화들의 리스트를 볼수있는, 
+-- 내즐겨찾기 화면에 필요한 SQL 문 작성. 
+
+
+-- 메인화면처럼 나오도록 하는 SQL 문 작성. 
+
+
+
+
+
 
 
 
