f52763e0a963c9356c45a51c661d39705310935a
blockenters <blockenters@gmail.com>
2023-12-07 15:55:54 +0900

    add

--- 04_server/01_recipe-server/app.py
+++ 04_server/01_recipe-server/app.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from config import Config
 
-from resources.recipe import RecipeListResource, RecipePublishResource, RecipeResource
+from resources.recipe import RecipeListResource, RecipeMeResource, RecipePublishResource, RecipeResource
 from resources.user import UserLoginResource, UserRegisterResource
 
 app = Flask(__name__)
@@ -25,6 +25,7 @@ api = Api(app)
 api.add_resource( RecipeListResource , '/recipes' )
 api.add_resource( RecipeResource  , '/recipes/<int:recipe_id>')
 api.add_resource( RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+api.add_resource( RecipeMeResource , '/recipes/me')
 api.add_resource( UserRegisterResource , '/user/register')
 api.add_resource( UserLoginResource , '/user/login')
 
