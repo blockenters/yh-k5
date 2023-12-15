83c3a93573e4bd3514fea199430105a77cc3e7d0
blockenters <blockenters@gmail.com>
2023-12-15 12:20:44 +0900

    add

--- 04_server/04_s3-image-upload/app.py
+++ 04_server/04_s3-image-upload/app.py
@@ -6,6 +6,7 @@ from flask_restful import Api
 from config import Config
 from resources.image import FileUploadResource
 from resources.posting import PostingResource
+from resources.rekognition import ObjectDetectionResource
 
 app = Flask(__name__)
 
@@ -19,6 +20,7 @@ api = Api(app)
 
 api.add_resource( FileUploadResource , '/upload')
 api.add_resource( PostingResource , '/posting')
+api.add_resource( ObjectDetectionResource , '/object_detection')
 
 
 def handler(event, context) :
