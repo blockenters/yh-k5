b65f6be7069a8f8508173bc692f2f456a1d6ad20
blockenters <blockenters@gmail.com>
2023-12-18 18:03:23 +0900

    add

---
+++ 04_server/06_posting-server/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager 
+from flask_restful import Api
+from config import Config
+from resources.posting import PostingListResource
+from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blocklist
+
+app = Flask(__name__)
+
+# 환경변수 셋팅
+app.config.from_object(Config)
+# JWT 매니저를 초기화 
+jwt = JWTManager(app)
+
+# 로그아웃된 토큰으로 요청하는 경우,
+# 실행되지 않도록 처리하는 코드.
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload): 
+    jti = jwt_payload['jti']
+    return jti in jwt_blocklist
+
+
+api = Api(app)
+
+api.add_resource( UserRegisterResource , '/user/register')
+api.add_resource( UserLoginResource, '/user/login')
+api.add_resource( UserLogoutResource , '/user/logout')
+api.add_resource( PostingListResource , '/posting')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
