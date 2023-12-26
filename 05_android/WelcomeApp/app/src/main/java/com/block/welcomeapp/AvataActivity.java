06c36b34c21a1ab35ec3b5bbc2bb027272818f48
blockenters <blockenters@gmail.com>
2023-12-26 16:10:31 +0900

    add

---
+++ 05_android/WelcomeApp/app/src/main/java/com/block/welcomeapp/AvataActivity.java
@@ -0,0 +1,14 @@
+package com.block.welcomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avata);
+    }
+}
